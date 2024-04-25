f5f09fe50ac6f50ae845da55f75e90b6ecb5c0ed
fadishafo <fadishafo@gmail.com>
2024-04-25 22:41:51 +0200

    Renommage des attributs Méthodes Check Victory

--- src/city.cpp
+++ src/city.cpp
@@ -1,13 +1,39 @@
 #pragma once
 #include "city.h"
 
-city::City(int victory, int  treasury, int shields, bool turn = false) : victory_points(victory), treasury(treasury), number_of_shields(shields), player_turn(turn), wonder(new Wonder* [4]), 
-PT(new ProgressToken* [10]), ressources(new Ressource* [30]), scientificsymbol(new ScientificSymbol* [7]),tailless(0),tailleressources(0) {
+//Constructeur
+city::City(int victory, int  treasury, int shields, bool turn = false) : victory_points(victory), treasury(treasury), number_of_shields(shields), player_turn(turn), wonder(new Wonder* [4]),
+progress_token(new ProgressToken* [10]), ressources(new Ressource* [30]), scientific_symbol(new ScientificSymbol* [7]),taille_scientific_symbol(0),taille_ressources(0) {
 }
 
+void city:: constructBuilding(){}
+    //Faire appel au consructeur de la classe Building
+void city:: constructWonder();{}
+    //Faire appel au consructeur de la classe Wonder
+void city:: discardCard(){}
 
+bool city:: checkMilitaryVictory(){
+    int military_position = 9;
+    if (number_of_shields >= military_position) {
+        std::cout << "Victoire militaire obtenue !" << std::endl;
+        return true;
+    } else {
+        std::cout << "Victoire militaire non atteinte." << std::endl;
+        return false;
+    }
+}
 
-
+bool city:: checkScientificVictory(){
+    //Ici il faut une fonction pour calculer
+    int unique_scientific_symbols = 6; //  Doit être calculé en fonction de la collection de symboles scientifiques.
+    if (taille_scientific_symbols >= unique_scientific_symbols) {
+        std::cout << "Victoire scientifique obtenue !" << std::endl;
+        return true;
+    } else {
+        std::cout << "Victoire scientifique non atteinte." << std::endl;
+        return false;
+    }
+}
 
 
 
--- src/city.h
+++ src/city.h
@@ -5,39 +5,66 @@
 #include"ProgressToken.h"
 
 class City {
-
-	int victory_points; 
+    Private:
+	int victory_points;
 	int treasury; 
 	int number_of_shields; 
 	bool player_turn;
 	Wonder** wonder; // 4 wonders
-	ProgressToken** PT; // max of 10  
+	ProgressToken** progress_token; // max of 10
 	Ressource** ressources;
-	int tailleressources ; 
-	int tailless; 
-	ScientificSymbol** scientificsymbol; //avoir un tableau de 7
-	City(int victory = 0, int  treasury = 0, int shields = 0, bool turn = false); 
+	int taille_ressources ;
+	int taille_scientific_symbol;
+	ScientificSymbol** scientific_symbol; //avoir un tableau de 7
+
+    Public:
+    //Contructeur
+	City(int victory = 0, int  treasury = 0, int shields = 0, bool turn = false);
+    //Destructeur
+    ~City() {
+        delete[] wonders; //delete wonder;
+        delete[] progress_tokens; //delete progress_token;
+
+        //A verifier le nombre de ressources pour
+        //Creer le destructeur et pouvoir
+        //Iterer sur le nombre de ressources
+
+        // Suppression des symboles scientifiques
+        for (int i = 0; i < taille_scientific_symbols; ++i) {
+            delete scientific_symbols[i];
+        }
+        delete[] scientific_symbols;
+        // Suppression des ressources
+        for (int i = 0; i < taille_ressources; ++i) {
+            delete ressources[i];
+        }
+        delete[] ressources;
+
+        /*  SI ON DÉCIDE QUE CE N'EST PAS UNE AGREGATION
+         *
+         *
+        // Suppression des merveilles
+        for (int i = 0; i < 4; ++i) {
+            delete wonders[i];
+        }
+        delete[] wonders;
+
+        // Suppression des jetons de progrès
+        for (int i = 0; i < 10; ++i) {
+            delete progress_tokens[i];
+        }
+        delete[] progress_tokens;
+         *
+         *
+         */
+    }
+
+    // Méthodes de la classe
 	void constructBuilding(); 
 	void constructWonder(); 
 	void discardCard(); 
 	bool checkMilitaryVictory(); 
-	bool checkScientificVictory(); 
-	~City() {
-		delete wonder;
-		delete PT;
-		//A verifier le nombre de ressources pour 
-		//Creer le destructeur et pouvoir 
-		//Iterer sur le nombre de ressources 
-		for (int i = 0; i++; i < tailless) {
-			delete scientificsymbol[i]; 
-		}
-		delete scientificsymbol;
-		for (int i = 0; i++; i < tailleressources) {
-			delete ressources[i];
-		}
-		delete ressources; 
-	}
-
+	bool checkScientificVictory();
 };
 
 
