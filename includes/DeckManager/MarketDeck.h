7e92546bac274b26a5d43335c8e01d68f79f6c4d
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2024-04-26 16:19:29 +0200

    Deck headers

--- includes/DeckManager/MarketDeck.h
+++ includes/DeckManager/MarketDeck.h
@@ -1,4 +1,49 @@
 #ifndef MARKETDECK_H
 #define MARKETDECK_H
+#include "DeckElement.h"
+
+namespace Deck {
+
+    class MarketDeck {
+
+        private:
+            const int age;
+            DeckElement** first_buildings;
+            const int nb_first_buildings;
+
+        public:
+            MarketDeck() :age(0), first_buildings(nullptr), nb_first_buildings(0){};
+            MarketDeck(const MarketDeck& m) = delete;
+            ~MarketDeck() {
+                for (int i = 0; i < nb_first_buildings; i++) {
+                    delete first_buildings[i];
+                }
+
+            };
+            MarketDeck& operator=(const MarketDeck& m) = delete;
+
+            void advanceAge();
+
+            class iterator {
+                private:
+                    DeckElement** building;
+                    unsigned int nb_buildings;
+                    friend class MarketDeck;
+                    iterator(DeckElement** building, const unsigned int& nb_buildings): building(building), nb_buildings(nb_buildings){};
+
+                public:
+                    iterator():building(nullptr), nb_buildings(0){};
+                    iterator& operator++() { building++; nb_buildings--; return *this; };
+                    bool operator!=(const iterator& i) const { return building != i.building; };
+                    iterator begin() { return iterator(building, nb_buildings); };
+                    iterator end() { return iterator(building + nb_buildings, 0); };
+                    DeckElement& operator*() { return **building; };
+                    // méthode pour accéder au building et le supprimer de l'architecture (rajoute les building fils dans la liste des choix possibles)
+                    Building& getBuilding();
+            };
+
+    };
+
+}
 
 #endif
