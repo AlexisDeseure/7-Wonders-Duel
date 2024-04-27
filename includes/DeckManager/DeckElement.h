85a4db4ac4323f8099faf39b713f088b4a6b5f04
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2024-04-27 15:44:35 +0200

    add attributes fathers for DeckElement class

--- includes/DeckManager/DeckElement.h
+++ includes/DeckManager/DeckElement.h
@@ -7,16 +7,20 @@ namespace Deck {
     private:
         const DeckElement* const right_son;
         const DeckElement* const left_son;
+        const DeckElement* const right_father;
+        const DeckElement* const left_father;
         const Building& building;
         bool visible;
 
     public:
-        DeckElement(const Building& building, const DeckElement* right_son = nullptr, const DeckElement* left_son = nullptr, const bool& visible = true): right_son(right_son), left_son(left_son), building(building), visible(visible){};
+        DeckElement(const Building& building, const bool& visible = true, const DeckElement* right_son = nullptr, const DeckElement* left_son = nullptr,  const DeckElement* right_father = nullptr,  const DeckElement* left_father = nullptr ): right_son(right_son), left_son(left_son), right_father(right_father), left_father(left_father), building(building), visible(visible){};
         ~DeckElement() = default;
         DeckElement& operator=(const DeckElement& d) = delete;
 
         const DeckElement* getRightSon() const { return right_son; };
         const DeckElement* getLeftSon() const { return left_son; };
+        const DeckElement* getRightFather() const { return right_father; };
+        const DeckElement* getLeftFather() const { return left_father; };
         const Building& getBuilding() const { return building; };
         bool isVisible() const { return visible; };
         void setVisible() { if (!visible) visible = true; };
