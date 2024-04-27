be714b69b1b37375acf7df25ec3ce002887f1b82
matmarcillo <marcillaudmateo@gmail.com>
2024-04-27 16:11:21 +0200

    Changement de la surcharge de ++ dans DeckElement.h On retourne le pointeur vers les fils Changement du signe de la surcharge à << et >>

--- includes/DeckManager/DeckElement.h
+++ includes/DeckManager/DeckElement.h
@@ -24,8 +24,8 @@ namespace Deck {
         const Building& getBuilding() const { return building; };
         bool isVisible() const { return visible; };
         void setVisible() { if (!visible) visible = true; };
-        DeckElement operator++(int) const { return *right_son; };
-        DeckElement operator++() const { return *left_son; };
+        DeckElement operator>>() const { return right_son; };
+        DeckElement operator<<() const { return left_son; };
 
     };
 
