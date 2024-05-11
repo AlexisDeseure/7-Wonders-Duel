75a4d90ae41e1eb5e2d9b056aa277e9985fc65ff
fadishafo <fadishafo@gmail.com>
2024-05-11 11:43:25 +0200

    Classe City avec transtion en vecteurs pour simplifier les choses et avancement sur les méthodes

--- includes/City/City.h
+++ includes/City/City.h
@@ -1,70 +1,44 @@
 #ifndef CITY_H
 #define CITY_H
 
-#define PROGRESS_TOKEN_LENGTH 10
-#define WONDER_LENGTH 4
-
+// peut-on gerer les includes comme ça Alexis ?
 #include <iostream>
+#include <vector>
+#include "../../includes/Cards/Wonder.h"
+#include "../../includes/Cards/Building.h"
+#include "../../includes/Cards/ProgressToken.h"
+#include "../../includes/City/Ressource.h"
+#include "../../includes/City/ScientificSymbol.h"
+#include "../../includes/Cards/Card.h"
 
-class Building;
-class Wonder;
-class ProgressToken;
-class Ressource;
-class ScientificSymbol;
-class Card;
 enum class RessourceType;
 enum class SymboleType;
 
 class City {
-
+private:
     int victory_points;
     int treasury;
     int number_of_shields;
     bool player_turn;
 
-    Wonder** wonder; // 4 wonders
-    ProgressToken** progress_tokens; // max of 10
-    Ressource** ressources; //5 ressources
-    ScientificSymbol** scientific_symbols; //avoir un tableau de 7
+    std::vector<Wonder*> wonders;
+    std::vector<ProgressToken*> progress_tokens;
+    std::vector<Ressource*> ressources;
+    std::vector<ScientificSymbol*> scientific_symbols;
+    std::vector<Building*> buildings;
 
 public:
-    City(int victory = 0, int  treasury = 0, int shields = 0, bool turn = false);
-    ~City() {
-        delete[] wonder;
-        delete[] progress_tokens;
-        //A verifier le nombre de ressources pour
-        //Creer le destructeur et pouvoir
-        //Iterer sur le nombre de ressources
-
-        delete[] scientific_symbols;
-        delete[] ressources;
-        /*  SI ON DÉCIDE QUE CE N'EST PAS UNE AGREGATION
-         *
-         *
-        // Suppression des merveilles
-        for (int i = 0; i < 4; ++i) {
-            delete wonders[i];
-        }
-        delete[] wonders;
-
-        // Suppression des jetons de progrès
-        for (int i = 0; i < 10; ++i) {
-            delete progress_tokens[i];
-        }
-        delete[] progress_tokens;
-         *
-         *
-         */
-    }
+    City(int victory = 0, int treasury = 0, int shields = 0, bool turn = false);
+    ~City();
     Ressource& getRessource(RessourceType name) const;
-    int getDistinctScientificSymbols() const{ return 0; };
-    bool canAfford(int price) const { return treasury >= price; };
+    int getDistinctScientificSymbols() const;
+    bool canAfford(int price) const;
     void constructBuilding(Building* building);
     void constructWonder(Wonder* wonder);
     void discardCard(Card* card);
     bool checkScientificVictory();
-    void addMoney(int money) { treasury += money; };
-    void addVictoryPoints(int points) { victory_points += points; };
-
+    void addMoney(int money);
+    void addVictoryPoints(int points);
 };
-#endif
+
+#endif // CITY_H
--- src/City/City.cpp
+++ src/City/City.cpp
@@ -1,28 +1,56 @@
 #include "../../includes/City/City.h"
-#include"../../includes/Cards/Wonder.h"
-#include"../../includes/Cards/Building.h"
-#include"../../includes/Cards/ProgressToken.h"
-#include"../../includes/City/Ressource.h"
-#include"../../includes/City/ScientificSymbol.h"
 
 City::City(int victory, int treasury, int shields, bool turn)
-    :victory_points(victory), treasury(treasury), number_of_shields(shields), player_turn(turn),
-    wonder(new Wonder *[WONDER_LENGTH]), progress_tokens(new ProgressToken *[PROGRESS_TOKEN_LENGTH]),
-    ressources(new Ressource*[static_cast<unsigned long long int>(RessourceType::RessourceTypeCount)]),
-    scientific_symbols(new ScientificSymbol*[static_cast<unsigned long long int>(SymboleType::SymbolTypeCount)])
-{}
+        : victory_points(victory), treasury(treasury), number_of_shields(shields), player_turn(turn) {}
+
+City::~City() {
+    // City détuit les objets pour les quelle elle est responsable de la mémoire
+    for (auto& w : wonders) {
+        delete w;
+    }
+    for (auto& p : progress_tokens) {
+        delete p;
+    }
+    for (auto& r : ressources) {
+        delete r;
+    }
+    for (auto& s : scientific_symbols) {
+        delete s;
+    }
+    for (auto& b : buildings) {
+        delete b;
+    }
+}
+
+Ressource& City::getRessource(RessourceType name) const {
+    for (auto& ressource : ressources) {
+        if (ressource->getType() == name) {
+            return *ressource;
+        }
+    }
+    throw std::invalid_argument("Ressource non trouvée.");
+}
+
+int City::getDistinctScientificSymbols() const {
+    std::set<SymboleType> symbols;
+    for (const auto& symbol : scientific_symbols) {
+        symbols.insert(symbol->getType());
+    }
+    return symbols.size();
+}
+
+bool City::canAfford(int price) const {
+    return treasury >= price;
+}
 
 void City::constructBuilding(Building* building) {
-    // Vérifie si le joueur a les ressources nécessaires
     int cost = building->getCost(this);
     if (canAfford(cost)) {
-        // Déduit les coûts des ressources
         treasury -= cost;
-        // Ajoute le bâtiment à la liste des bâtiments de la cité
-        // A VOIR COMMENT
-        std::cout << "Bâtiment construit : " << building->getName() << std::endl;
+        buildings.push_back(building);
+        std::cout << "Building construit: " << building->getName() << std::endl;
     } else {
-        std::cout << "Ressources insuffisantes pour construire ce bâtiment." << std::endl;
+        std::cout << "Ressources insuffisantes." << std::endl;
     }
 }
 
@@ -31,59 +59,32 @@ void City::constructWonder(Wonder* wonder) {
     if (canAfford(cost) && !wonder->isBuilt()) {
         treasury -= cost;
         wonder->setBuilt(true);
-        std::cout << "Merveille construite : " << wonder->getName() << std::endl;
-        // Active l'effet de la merveille si nécessaire
-        // A FAIRE
+        wonders.push_back(wonder);
+        std::cout << "Wonder consuite: " << wonder->getName() << std::endl;
     } else {
-        std::cout << "Conditions non remplies pour construire cette merveille." << std::endl;
+        std::cout << "Conditions non accomples pour constuire la wonder." << std::endl;
     }
 }
 
 void City::discardCard(Card* card) {
-    int coinsReceived = 2; // Base de pièces reçues pour défausser une carte
-    // Ajoute des pièces supplémentaires si des bâtiments commerciaux spécifiques sont présents
+    int coinsReceived = 2; // Base amount for discarding a card
+
+     // Ajoute des pièces supplémentaires si des bâtiments commerciaux spécifiques sont présents
     coinsReceived += 0;//calculer coins from commercialBuildings();
+
     treasury += coinsReceived;
-    std::cout << "Carte défaussée pour " << coinsReceived << " pièces." << std::endl;
-    // La carte est ensuite retirée du jeu ou placée dans une pile de défausse
-    // A VOIR COMMENT
+    std::cout << "Card discarded for " << coinsReceived << " coins." << std::endl;
+    // Handle card removal or placement into a discard pile if necessary
 }
 
-
-/* je pense qu'il faudrait plutôt faire ça au niveau de la classe board qui gère le jeu : par exemple à chaque tour, elle interroge l'instance
- * de la classe ConflictPawn pour savoir si un des 2 joueurs a gagné la victoire militaire
-    bool City::checkMilitaryVictory() {
-        if (military_position >= 9 ) {
-            std::cout << "Victoire militaire obtenue !" << std::endl;
-            return true;
-        }
-        else {
-            std::cout << "Victoire militaire non atteinte." << std::endl;
-            return false;
-        }
-    }
-*/
 bool City::checkScientificVictory() {
-    //Ici il faut une fonction pour calculer
-    if (getDistinctScientificSymbols() >= static_cast<unsigned long long int>(SymboleType::SymbolTypeCount)-1) {
-        std::cout << "Victoire scientifique obtenue !" << std::endl;
-        return true;
-    }
-    else {
-        std::cout << "Victoire scientifique non atteinte." << std::endl;
-        return false;
-    }
-
-
+    return getDistinctScientificSymbols() >= 6; // Il y a symboles 7 distinct dans le jeu
 }
 
-Ressource& City::getRessource(RessourceType name) const {
-    for (int i = 0; i < static_cast<unsigned long long int>(RessourceType::RessourceTypeCount); ++i) {
-        if (ressources[i]->getType() == name) {
-            return *ressources[i];
-        }
-    }
-    throw std::invalid_argument("Ressource non trouvée.");
+void City::addMoney(int money) {
+    treasury += money;
 }
 
-
+void City::addVictoryPoints(int points) {
+    victory_points += points;
+}
