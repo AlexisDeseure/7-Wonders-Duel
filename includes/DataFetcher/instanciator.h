f4d5d7f1e501287a614da12319cc624929f7085d
Bastien Cuvillier <148553617+Voln1x@users.noreply.github.com>
2024-05-21 12:10:06 +0200

    Avancements sur instanciator.h et instanciator.cpp

--- includes/DataFetcher/instanciator.h
+++ includes/DataFetcher/instanciator.h
@@ -1,24 +1,51 @@
 #ifndef INSTANCIATOR_H
 #define INSTANCIATOR_H
 
-#include "../../Cards/Card.h"
+#include "../../Cards/Building.h"
+#include "../../Cards/Wonder.h"
+#include "../../Cards/ProgressToken.h"
 #include "../../DataFetcher/File.h"
 
 class Instanciator
 {
 private:
-    std::vector<Card> cartes_buildings;
+    // structure de données à revoir en fonction de votre implémentation.
+    std::vector<Building> buildings_instanciator;
+    std::vector<Wonder> wonders_instanciator;
+    std::vector<ProgressToken> progress_tokens_instanciator;
     File json;
     std::vector<std::pair<QString,QString>> names;
-public:
-    Instanciator(): cartes_buildings() {
-        names = json.getNames();
-    };
-    std::vector<Card>& getBuildings() {return cartes_buildings;};
-    std::vector<std::pair<QString,QString>> getNames() {return names;}
 
-    void addBuilding(Card carte);
-    Card instantCard(QString name);
+    // On souhaite instancier une seule fois, on met en privé les méthodes qui permettent de créer les instances.
+    void addBuildingToInstanciator(Building building);
+    void addPTtoInstanciator(ProgressToken PT);
+    void addWonderToInstanciator(Wonder wonder);
+
+    // Créateur des instances en privé pour la même raison.
+    void constructBuilding();
+    void constructWonder();
+    void constructPT();
+
+public:
+
+    //Instanciation directement dans le constructeur.
+    Instanciator(): buildings_instanciator(),wonders_instanciator(),progress_tokens_instanciator() {
+        names = json.getNames();
+        constructBuilding();
+        constructWonder();
+        constructPT();
+    };
+
+    //Interdiction de la recopie à défaut de faire un singleton pour l'instant.
+    Instanciator& operator=(const Instanciator& x) = delete;
+
+    //L'Utilisateur pourra seulement avoir accès aux vecteurs des cartes plus haut.
+    std::vector<Building>& getBuildings() {return buildings_instanciator;};
+    std::vector<Wonder>& getWonders() {return wonders_instanciator;};
+    std::vector<ProgressToken> progress_token_instanciator() {return progress_tokens_instanciator;};
+
+    //On peut laisser la possibilité de donner la liste des cartes si besoin.
+    std::vector<std::pair<QString,QString>>& getNames() {return names;}
 };
 
 #endif // INSTANCIATOR_H
--- src/DataFetcher/instanciator.cpp
+++ src/DataFetcher/instanciator.cpp
@@ -1,2 +1,12 @@
 #include "../../includes/DataFetcher/Instanciator.h"
 
+void Instanciator::constructBuilding(){
+    std::vector<std::pair<QString,QString>> Lnames=getNames();
+    for (std::pair<QString,QString>& noms : Lnames) {
+        if (noms.first == "Building"){
+            std::vector<Building> Buildings = getBuildings();
+
+        }
+    }
+
+}
