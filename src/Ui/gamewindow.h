652c0569bf5fafd659ef7594cd0cea3e344c0ce9
Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>
2024-05-25 00:41:40 +0200

    Update gamewindow.h

--- src/Ui/gamewindow.h
+++ src/Ui/gamewindow.h
@@ -1,1 +1,30 @@
+#ifndef GAMEWINDOW_H
+#define GAMEWINDOW_H
 
+#include <QMainWindow>
+#include "playerwidget.h"
+#include "cardwidget.h"
+#include "Game.h" //  game header
+
+class GameWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit GameWindow(QWidget *parent = nullptr);
+    ~GameWindow();
+
+private:
+    PlayerWidget *player1Widget;
+    PlayerWidget *player2Widget;
+    QList<CardWidget*> cardWidgets;
+
+    void setupUi();
+    void setupConnections();
+    Game game; // game instance
+
+public slots:
+    void onCardClicked();
+};
+
+#endif // GAMEWINDOW_H
