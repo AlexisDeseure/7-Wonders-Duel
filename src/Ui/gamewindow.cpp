022cff96fafef7384a34f4684af4ec68edbe08bc
Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>
2024-05-25 00:49:09 +0200

    Add of example of cards and ressources in gamewindow.cpp

--- src/Ui/gamewindow.cpp
+++ src/Ui/gamewindow.cpp
@@ -48,3 +48,39 @@ void GameWindow::onCardClicked()
     // TODO : Handle card click events and interact with the game logic
 }
 
+
+void GameWindow::setupUi()
+{
+    player1Widget = new PlayerWidget(this);
+    player2Widget = new PlayerWidget(this);
+
+    QWidget *centralWidget = new QWidget(this);
+    QVBoxLayout *mainLayout = new QVBoxLayout(centralWidget);
+
+    mainLayout->addWidget(player1Widget);
+    mainLayout->addWidget(player2Widget);
+
+    QHBoxLayout *cardsLayout = new QHBoxLayout();
+    for (int i = 0; i < 5; ++i) {
+        CardWidget *cardWidget = new CardWidget(this);
+        cardWidget->setCardInfo(QString("Card %1").arg(i + 1), "Effect Example");
+        cardWidgets.append(cardWidget);
+        cardsLayout->addWidget(cardWidget);
+    }
+    mainLayout->addLayout(cardsLayout);
+
+    setCentralWidget(centralWidget);
+
+    // Example: Initialize player resources and points
+    QMap<QString, int> initialResources;
+    initialResources["Wood"] = 3;
+    initialResources["Stone"] = 2;
+
+    player1Widget->updateResources(initialResources);
+    player1Widget->updateVictoryPoints(5);
+
+    player2Widget->updateResources(initialResources);
+    player2Widget->updateVictoryPoints(7);
+}
+
+
