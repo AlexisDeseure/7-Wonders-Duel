ea937c5c3fef1ccdcd7be84a702c1d234434e89b
Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>
2024-05-25 00:43:39 +0200

    Update playerwidget.h

--- src/Ui/playerwidget.h
+++ src/Ui/playerwidget.h
@@ -1,1 +1,23 @@
+#ifndef PLAYERWIDGET_H
+#define PLAYERWIDGET_H
 
+#include <QWidget>
+#include <QLabel>
+#include <QMap>
+
+class PlayerWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit PlayerWidget(QWidget *parent = nullptr);
+
+    void updateResources(const QMap<QString, int> &resources);
+    void updateVictoryPoints(int points);
+
+private:
+    QLabel *resourcesLabel;
+    QLabel *victoryPointsLabel;
+};
+
+#endif // PLAYERWIDGET_H
