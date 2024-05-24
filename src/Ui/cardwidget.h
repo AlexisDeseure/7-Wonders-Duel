54b71a76f51e0620cfeca75374b98278c10f7475
Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>
2024-05-25 00:45:08 +0200

    Update cardwidget.h

--- src/Ui/cardwidget.h
+++ src/Ui/cardwidget.h
@@ -1,1 +1,26 @@
+#ifndef CARDWIDGET_H
+#define CARDWIDGET_H
 
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+
+class CardWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CardWidget(QWidget *parent = nullptr);
+    void setCardInfo(const QString &name, const QString &effect);
+    QString getCardName() const;
+
+signals:
+    void cardClicked();
+
+private:
+    QLabel *nameLabel;
+    QLabel *effectLabel;
+    QPushButton *selectButton;
+};
+
+#endif // CARDWIDGET_H
