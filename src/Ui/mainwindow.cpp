7c02d1de864700c3e44f3ad2be07f6ae969acbf1
Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>
2024-05-25 00:40:19 +0200

    Update mainwindow.cpp

--- src/Ui/mainwindow.cpp
+++ src/Ui/mainwindow.cpp
@@ -1,1 +1,33 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
 
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , gameWindow(nullptr)
+{
+    ui->setupUi(this);
+
+    connect(ui->startButton, &QPushButton::clicked, this, &MainWindow::startNewGame);
+    connect(ui->loadButton, &QPushButton::clicked, this, &MainWindow::loadGame);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    if (gameWindow) {
+        delete gameWindow;
+    }
+}
+
+void MainWindow::startNewGame()
+{
+    gameWindow = new GameWindow(this);
+    gameWindow->show();
+    this->hide();
+}
+
+void MainWindow::loadGame()
+{
+    // TODO:  load an existing game with the fonction game
+}
