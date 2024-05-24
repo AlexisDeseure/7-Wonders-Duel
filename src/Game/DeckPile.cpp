3d3193060bfd5e17f9a64f44adcaf1a66fed2596
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2024-05-24 15:30:36 +0200

    deckpile constructor

--- src/Game/DeckPile.cpp
+++ src/Game/DeckPile.cpp
@@ -2,5 +2,9 @@
 #include "Instanciator.h"
 
 DeckPile::DeckPile(int nb_buildings, int nb_token, int nb_wonder){
-
+    wonders = Instanciator::getInstanciator()->extractXRandomWondersFrom(Instanciator::getInstanciator()->getWonders(), nb_wonder);
+    buildings.first = Instanciator::getInstanciator()->extractXRandomBuildingsFrom(Instanciator::getInstanciator()->getBuildings(), nb_buildings);
+    progress_tokens = Instanciator::getInstanciator()->extractXRandomBuildingsFrom(Instanciator::getInstanciator()->getBuildings(), nb_buildings);
 }
+
+
