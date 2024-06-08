59b52e9678d9029eb26fc9f718a0372582468471
Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>
2024-06-08 13:43:48 +0200

    Connector::update

--- src/Ui/Connector.cpp
+++ src/Ui/Connector.cpp
@@ -4,3 +4,11 @@
 void StartMenuConnector::startButtonHandler(){
     qDebug() << "START PRESSED";
 }
+
+Connector::Connector(Game* game) : game(game) {
+    game->attach(this);
+}
+
+void Connector::update(Subject* subject) {
+    // Update UI 
+}
