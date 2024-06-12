42b65b70e9611edcb16e68b0c971b852a9891791
matmarcillo <130035308+matmarcillo@users.noreply.github.com>
2024-06-12 15:19:10 +0200

    compiled version of playerwidget instantiation
    Co-Authored-By: Fadi Shafo <128102804+FadiShafo@users.noreply.github.com>

--- includes/Ui/playerwidget.h
+++ includes/Ui/playerwidget.h
@@ -19,6 +19,8 @@ public:
 
     Player* getplayer() const{return player;};
 
+    void updatePlayerInfo();
+
 signals:
     void onRefreshButtonClicked();
 private:
--- src/Ui/playerwidget.cpp
+++ src/Ui/playerwidget.cpp
@@ -1,20 +1,20 @@
 #include "PlayerWidget.h"
 #include "Player.h"
-#include
+#include "City.h"
 #include <QVBoxLayout>
 #include <QString>
 
 PlayerWidget::PlayerWidget(Player* p, QWidget* parent) : QWidget(parent), player(p) {
     playerName = new QLabel(QString::fromStdString(player->getName()), this);
     playerAIStatus = new QLabel(QString("AI Level: ") + QString::fromStdString(AiLeveltoString(player->getAiLevel())), this);
-    playerCityTreasury = new QLabel(QString("Treasury: ") + QString::number(getplayer()->getCity()->getTreasury()), this);
+    playerCityTreasury = new QLabel(QString("Treasury: ") + QString::number(getplayer()->getCity().getTreasury()), this);
     playerCityShields = new QLabel(QString("Shields: ") + QString::number(player->getCity().getShields()), this);
     playerCityVictoryPoints = new QLabel(QString("Victory Points: ") + QString::number(player->getCity().getVictoryPoints()), this);
 
     refreshButton = new QPushButton("Refresh", this);
     connect(refreshButton, &QPushButton::clicked, this, &PlayerWidget::onRefreshButtonClicked);
 
-    QVBoxLayout layout = new QVBoxLayout(this);
+    QVBoxLayout* layout = new QVBoxLayout(this);
     layout->addWidget(playerName);
     layout->addWidget(playerAIStatus);
     layout->addWidget(playerCityTreasury);
