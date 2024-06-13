27e0441b4a0a05ae9452bb85852ef1ebfddfdc0d
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2024-06-13 19:48:09 +0200

    add wonders and tokens pictures

--- src/Ui/marketdeckwidget.cpp
+++ src/Ui/marketdeckwidget.cpp
@@ -2,6 +2,7 @@
 #include "cardwidget.h"
 #include "BuildingsLayout.h"
 #include "Card.h"
+#include "Instanciator.h"
 #include <vector>
 
 std::vector<Building*> buildings; //à rajouter dans le main
@@ -25,16 +26,14 @@ bool MarketDeckWidget::checkCardPos(int age,int i,int j)const{
 void MarketDeckWidget::generateAge(int age){
     delete this->Cardlines;
     Cardlines = new QVBoxLayout(this);
-<<<<<<< Updated upstream
 
     int width = 115;
     int height = 170;
 
-    std::vector<std::vector<DeckElement*>>& layout = BuildingsLayout::getAgeWithBuildings(age, buildings); //buildings à rajouter comme var statique
-=======
+    // std::vector<std::vector<DeckElement*>>& layout = Instanciator::getInstanciator()->getBuildingsLayout().getAgeWithBuildings(age, buildings); //buildings à rajouter comme var statique
+    // DeckElement* building;
+
     std::vector<std::vector<DeckElement*>>& layout = market->getAllBuildings(); //buildings à rajouter comme var statique
->>>>>>> Stashed changes
-    DeckElement* building;
 
     switch(age){
     case 1: //10 cases de long, 5 lignes
