3461bc9ec19921c88cc3a729673be4d1653de4f3
AlexisDeseure <98423557+AlexisDeseure@users.noreply.github.com>
2024-06-15 15:57:55 +0200

    instanciate all buildings directly

--- includes/Game/DeckPile.h
+++ includes/Game/DeckPile.h
@@ -10,10 +10,11 @@ class ProgressToken;
 
 class DeckPile{
 private:
+
     std::pair<std::vector<Building*>, std::vector<Building*>> buildings;
     std::vector<Wonder*> wonders;
     std::pair<std::vector<ProgressToken*>,std::vector<ProgressToken*>> progress_tokens;
-
+    std::vector<std::vector<Building*>> all_buildings;
     int nb_buildings;
 
 public:
@@ -29,7 +30,7 @@ public:
     std::vector<ProgressToken*>& getOtherProgressTokens() {return progress_tokens.second;};
     std::vector<Building*>& getBuildings() {return buildings.first;};
     std::vector<ProgressToken*>& getProgressTokens() {return progress_tokens.first;};
-
+    std::vector<std::vector<Building*>> getAllBuildings() {return all_buildings;};
     void advanceAge(int age);
 };
 
